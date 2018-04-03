969f84e7f9fbabe83b8d826aec322bb9908fdd35
Vel <protsan2707@gmail.com>
2018-04-03 14:59:30 +0300

    Add container

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,24 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
         //Less to css
-      less: {
-          development: {
-              options: {
-                  paths: ['src/styles'],
-                  compress: false,
-                  yuicompress: true,
-                  optimization: 2
-              },
-              files: {
-                  'dist/css/style.css': 'src/styles/style.less'
-              }
-          }
-      }
+        less: {
+            development: {
+                options: {
+                    paths: ['src/styles']
+                },
+                files: {
+                    'dist/css/style.css': 'src/styles/style.less'
+                }
+            }
+        }
+    })
 
-
-  });
-
-    //Load Tasks
+    //Load task
     grunt.loadNpmTasks('grunt-contrib-less');
 
     //Register Tasks
     grunt.registerTask('default', ['less']);
+
 };
